0b3a2ce9499bd68987216f44dca3ae350eaf2bb1
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-02-23 23:26:42 +0100

    test(h1): improve

--- main_test.go
+++ main_test.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestIsH1(t *testing.T) {
-	str := "# Title"
-	result := isH1(str)
-	if !isH1(str) {
-		t.Errorf("isH1(\"%v\") = %v; want true", str, result)
-	}
+	var tests = []struct {
+        str string
+        want bool
+    }{
+        {"# Title", true},
+        {"#Title", false},
+    }
+    for _, tt := range tests {
+        testname := fmt.Sprintf("%s", tt.str)
+        t.Run(testname, func(t *testing.T) {
+            result := isH1(tt.str)
+            if tt.want != result {
+                t.Errorf("want %t, got %t", tt.want, result)
+            }
+        })
+    }
 }
