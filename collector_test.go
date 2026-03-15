3d416c057c97e17b59fc39532cfbf6cd8ca0efe9
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-03-15 15:59:48 +0100

    test(collector): update

--- collector_test.go
+++ collector_test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TestCollectFiles(t *testing.T) {
-	got, err := collectFiles("testdata/md")
+	got, err := collectFiles("testdata/dir-to-convert/content")
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 	want := []conversionFile{
-		{input: "testdata/md/file.md", output: "testdata/md/file.html"},
-		{input: "testdata/md/subfolder/file-b.md", output: "testdata/md/subfolder/file-b.html"},
+		{input: "testdata/dir-to-convert/content/file.md", output: "testdata/dir-to-convert/content/file.html"},
+		{input: "testdata/dir-to-convert/content/subfolder/file-b.md", output: "testdata/dir-to-convert/content/subfolder/file-b.html"},
 	}
 	if len(got) != len(want) {
 		t.Fatalf("expected %d files, got %d", len(want), len(got))
